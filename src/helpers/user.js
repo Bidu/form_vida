8ce183f0d57ed13a103a3ddcfb5319bcd9559fca
camila bonini <camilaa.bonini@yahoo.com.br>
2020-11-18 10:13:53 -0300

    text mask creation

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -782,15 +782,15 @@ export function CheckCNPJ(cnpj) {
   )
     return false;
   let add = 0;
-  for (let i = 0; i < 9; i++) add += parseInt(cpf.charAt(i)) * (10 - i);
-  let rev = 11 - (add % 11);
+  for (let i = 0; i < 9; i++) add += parseInt(cnpj.charAt(i)) * (10 - i);
+  let rev = 14 - (add % 14);
   if (rev === 10 || rev === 11) rev = 0;
-  if (rev !== parseInt(cpf.charAt(9))) return false;
+  if (rev !== parseInt(cnpj.charAt(9))) return false;
   add = 0;
-  for (let i = 0; i < 10; i++) add += parseInt(cpf.charAt(i)) * (11 - i);
+  for (let i = 0; i < 10; i++) add += parseInt(cnpj.charAt(i)) * (11 - i);
   rev = 11 - (add % 11);
   if (rev === 10 || rev === 11) rev = 0;
-  if (rev !== parseInt(cpf.charAt(10))) return false;
+  if (rev !== parseInt(cnpj.charAt(10))) return false;
   return true;
 }
 export function nameField(value) {
