8c7d92d2b132a80019da5de22996f68f824a5250
Henrique França <euhenriquefranca@gmail.com>
2020-11-30 11:07:07 -0300

    Campo data de nascimento criado

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -1,15 +1,32 @@
-
-import * as React from 'react';
-import { Pickers } from '@material-ui/pickers ';
-<form className={classes.container} noValidate>
-  <TextField
-    id="date"
-    label="Birthday"
-    type="date"
-    defaultValue="2017-05-24"
-    className={classes.textField}
-    InputLabelProps={{
-      shrink: true,
-    }}
-  />
-</form>
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+export default function DatePickers() {
+  const classes = useStyles();
+  return (
+    <form className={classes.container} noValidate>
+      <TextField
+        id="birth"
+        label="Nascimento"
+        type="date"
+        defaultValue="2017-05-24"
+        className={classes.textField}
+        style={{marginLeft: "0px"}}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+  );
+}
