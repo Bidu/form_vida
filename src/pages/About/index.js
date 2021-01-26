2d0dbb97ed4614675a9764ee221e08958d520ce0
camila bonini <camilaa.bonini@yahoo.com.br>
2021-01-26 18:51:42 -0300

    retirada do botão voltar about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1093,11 +1093,11 @@ class About extends Component {
                 </>
     
           </form>
-          <div className="actions mt0">
+          {/* <div className="actions mt0">
             <Link className="btn-back" to="/">
               <KeyboardBackspaceIcon /> Voltar
             </Link>
-          </div>
+          </div> */}
         </Wrapper>
       </>
     );
