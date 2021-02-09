6a8be5834e70284760da3fe30539591b9b7e989f
camila bonini <camilaa.bonini@yahoo.com.br>
2021-02-09 16:31:27 -0300

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,10 +17,10 @@ const Routes = () => (
         <Redirect to="/sobre-voce" />
       </Route>
       <Route exact path="/sobre-voce" component={About} forceRefresh={true} />
-      <Route exact path="/cotacao" component={PriceQuote} />
-      <Route exact path="/questionario" component={Questionario} />
-      <Route exact path="/checkout" component={Checkout} />
-      <Route exact path="/sucesso" component={Success} />
+      <Route exact path="/cotacao/:id" component={PriceQuote} />
+      <Route exact path="/questionario/:id" component={Questionario} />
+      <Route exact path="/checkout/:id" component={Checkout} />
+      <Route exact path="/sucesso/:id" component={Success} />
     </Switch>
   </BrowserRouter>
 );
